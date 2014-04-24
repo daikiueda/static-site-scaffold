4cc97ce4d12d35ddb1397b2dc8179cd7402890f3
daikiueda <daikiueda@users.noreply.github.com>
2014-04-25 06:59:57 +0900

    ついでにテストコードを書き足した。

--- assets/test/common/siteScript/$.namespace.spec.browserify.js
+++ assets/test/common/siteScript/$.namespace.spec.browserify.js
@@ -1,7 +1,6 @@
 "use strict";
 
-var chai = chai || require( "chai" ),
-    expect = chai.expect,
+var expect = chai.expect,
     fixtures = require( "js-fixtures" ),
 
     ERRORS_STOCK_SCRIPT_TAG = [
@@ -15,6 +14,12 @@ var chai = chai || require( "chai" ),
 describe( "siteScript", function(){
 
     describe( "$.namespace", function(){
+        it( "名前空間を定義する。", function(){
+            expect( $.namespace.getRoot().test1 ).to.not.exist;
+            $.namespace( "siteScript.test1.test2.test3" );
+            expect( $.namespace.getRoot().test1.test2.test3 ).to.exist;
+        } );
+
         describe( "ファイルの読み込み順に関して", function(){
             if( typeof window === "undefined" ){
                 it( "ブラウザでないとテストできません。" );
@@ -22,13 +27,51 @@ describe( "siteScript", function(){
 
                 afterEach( function(){
                     fixtures.cleanUp();
-                    fixtures.clearCache();
+                } );
+
+                it( "$.namespaceが読み込まれた時に、window.$が存在する場合は、そのメンバーとしてnamespaceメソッドを定義する。", function( done ){
+                    fixtures.set( [
+                        ERRORS_STOCK_SCRIPT_TAG,
+                        '<script src="/bower_components/jquery/dist/jquery.js"></script>',
+                        '<script src="/js/common/siteScript/$.namespace.js"></script>'
+                    ].join( "" ) );
+
+                    fixtures.window().onload = function(){
+                        expect( fixtures.window().jQuery.namespace ).to.be.a( "function" );
+                        done();
+                    };
+                } );
+
+                it( "$.namespaceが読み込まれた時に、window.$が存在しない場合は、まずグローバルスコープに$を定義して、そのメンバーとしてnamespaceメソッドを定義する。", function( done ){
+                    fixtures.set( [
+                        ERRORS_STOCK_SCRIPT_TAG,
+                        '<script src="/js/common/siteScript/$.namespace.js"></script>'
+                    ].join( "" ) );
+
+                    fixtures.window().onload = function(){
+                        expect( fixtures.window().$.namespace ).to.be.a( "function" );
+                        done();
+                    };
                 } );
 
                 it( "$.namespaceが読み込まれた時に、すでに同名のプロパティが存在する場合は、エラーを投げる。", function( done ){
                     fixtures.set( [
                         ERRORS_STOCK_SCRIPT_TAG,
                         '<script>var $ = { namespace: function(){} };</script>',
+                        '<script src="/js/common/siteScript/$.namespace.js"></script>'
+                    ].join( "" ) );
+
+                    fixtures.window().onload = function(){
+                        expect( fixtures.window().errorsStock[ 0 ] ).to
+                            .contain( "$.namespaceの定義に失敗しました。scriptファイルの構成が想定外です。" );
+                        done();
+                    };
+                } );
+
+                it( "$.namespaceが複数回定義された場合、エラーを投げる。", function( done ){
+                    fixtures.set( [
+                        ERRORS_STOCK_SCRIPT_TAG,
+                        '<script src="/js/common/siteScript/$.namespace.js"></script>',
                         '<script src="/js/common/siteScript/$.namespace.js"></script>'
                     ].join( "" ) );
 
