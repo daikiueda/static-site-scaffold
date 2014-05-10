f75677094c96caf6345bf424dfa7844e6a4c9b3b
daikiueda, @ue_di <daikiueda@users.noreply.github.com>
2014-05-10 21:45:21 +0900

    #11 karma( and Testem )の設定ミス。

--- assets/test/karma.conf.js
+++ assets/test/karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function(config) {
 
     // list of files to exclude
     exclude: [
-      'test/__scaffold/**/*.conf.js',
+      'test/__scaffold/**/*.js',
       'test/**/*.conf.js',
       'test/**/*browserify*.js',
       'test/**/testem.*.js'
